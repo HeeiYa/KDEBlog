30ddbba347499cf83002463ca15dbf6f1813da4a
HeeiYa <90752469+HeeiYa@users.noreply.github.com>
2024-07-03 11:57:32 +0800

    fix css

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,6 +10,7 @@ export default defineUserConfig({
   description: '',
 
   theme: defaultTheme({
+    base:'/KDEBlog/',
     logo: 'https://avatars.githubusercontent.com/u/90752469?s=400&u=69796f3c09ffaa19d5dc128699599458dadb5d14&v=4',
 
     navbar: [
